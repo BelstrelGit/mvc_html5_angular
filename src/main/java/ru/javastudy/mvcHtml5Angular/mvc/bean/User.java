2f916a484e0b3fdca65a4763e13a79fae7237a09
BelstrelGit <Belstrel@gmail.com>
2017-10-17 16:54:06 +0300

    Hibernate 5

--- src/main/java/ru/javastudy/mvcHtml5Angular/mvc/bean/User.java
+++ src/main/java/ru/javastudy/mvcHtml5Angular/mvc/bean/User.java
@@ -1,13 +1,36 @@
 package ru.javastudy.mvcHtml5Angular.mvc.bean;
 
+import com.sun.javafx.beans.IDProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.annotation.Generated;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
 /**
  * Created by belstrel on 09.10.17.
  */
+@Entity
+@Table(name="USER")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IDUSER")
     private int idUser;
+
+    @NotEmpty
+    @Size(min = 5, max = 20)
+    @Column(name = "USERNAME")
     private String username;
+
+    @NotEmpty
+    @Size(min=5, max=20)
+    @Column(name="PASSWORD")
     private String password;
+
+    @Column(name="ENABLED")
     private boolean enabled;
+
 
     public int getIdUser() {
         return idUser;
